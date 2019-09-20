9f9c62ae3c0a5de98a32a4e915750cbb58831cca
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-20 19:28:52 +0200

    build: add debug traces

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('require-one-of')
 const humanizeList = require('humanize-list')
 const resolveFrom = require('resolve-from')
 const assert = require('assert')
@@ -23,15 +24,29 @@ const relativeNodeModulesPath = path.resolve(__dirname, '..', '..')
  * This is not applied when you are running it using `npx`
  * In that case, node_modules is relative to the dependency itself.
  */
-const resolveModule = module =>
-  resolveFrom.silent(process.cwd(), module) || resolveFrom.silent(relativeNodeModulesPath, module)
+const resolveModule = module => {
+  const relativeToProcess = resolveFrom.silent(process.cwd(), module)
+  debug(` - ${module} at ${process.cwd()} → ${!!relativeToProcess}`)
+  if (relativeToProcess) return relativeToProcess
+
+  const relativeToPath = resolveFrom.silent(relativeNodeModulesPath, module)
+  debug(` - ${module} at ${relativeNodeModulesPath} → ${!!relativeToPath}`)
+  if (relativeToProcess) return relativeToPath
+
+  return undefined
+}
 
 const find = (modules, error = createError) => {
+  debug(`searching ${modules}`)
   for (const module of modules) {
     const modulePath = resolveModule(module)
-    if (modulePath) return require(modulePath)
+    if (modulePath) {
+      debug(`found ${module} at ${modulePath}`)
+      return require(modulePath)
+    }
   }
 
+  debug(`${modules} not found, throwing an error`)
   throw error(modules)
 }
 
